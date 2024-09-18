97f6352d3d7633e12a8ed14edce88506156d2562
Francislainy Campos <francislainy.campos@gmail.com>
2024-09-18 15:37:48 +0100

    Add assets include

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/portfolio/',
   plugins: [react()],
+  assetsInclude: ['**/*.jpg', '**/*.png', '**/*.svg', '**/*.gif'],
 });
